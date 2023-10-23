b9d421565ba5dfcb31b07b0969fe4b692fa359b9
SrMiranha <132907964+SrMiranha@users.noreply.github.com>
2023-10-23 08:08:21 -0300

    Add files via upload

---
+++ a.py
@@ -0,0 +1,23 @@
+import turtle
+
+#1 
+for x in range (4):
+    turtle.forward(200)
+    turtle.left(90)
+
+#2 
+for x in range (4):
+    turtle.forward(200)
+    turtle.right(90)
+
+#3  
+for x in range (4):
+    turtle.forward(300)
+    turtle.right(90)
+
+#4
+for x in range (2):
+     turtle.forward(90)
+     turtle.left(90)
+     turtle.forward(200)
+     turtle.left(90)
